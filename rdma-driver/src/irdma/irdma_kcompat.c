jakemoroni/ethernet-linux-irdma-and-idpf

commit 28d5244aaa8b1797bd6520711459c540b8b58b8a

irdma: enable full AH dedup with deferred deletion
Creating address handles can be expensive, and in some
cases, it is done while inside of an atomic section.

To mitigate this, the address handles can be deduplicated
in the driver to avoid the extra CQP transactions.

Since RDMA CM creates many short-lived address handles,
the deletion of an AH must be deferred in order to
maximize the hit rate, so a 5 second deletion delay
was implemented.

Signed-off-by: Jacob Moroni <jmoroni@google.com>

rdma-driver/src/irdma/irdma_kcompat.c
@@ -1015,22 +1015,32 @@ static bool irdma_ah_exists(struct irdma_device *iwdev,
 {
 	struct irdma_ah *ah;
 	u32 save_ah_id = new_ah->sc_ah.ah_info.ah_idx;
+	u32 save_flow_label = new_ah->sc_ah.ah_info.flow_label;
 	u32 key = new_ah->sc_ah.ah_info.dest_ip_addr[0] ^
 		  new_ah->sc_ah.ah_info.dest_ip_addr[1] ^
 		  new_ah->sc_ah.ah_info.dest_ip_addr[2] ^
 		  new_ah->sc_ah.ah_info.dest_ip_addr[3];
 
 	hash_for_each_possible(iwdev->ah_hash_tbl, ah, list, key) {
-		/* Set ah_id the same so memcp can work */
+		/* Set ah_id the same so memcpy can work */
 		new_ah->sc_ah.ah_info.ah_idx = ah->sc_ah.ah_info.ah_idx;
+		new_ah->sc_ah.ah_info.flow_label = ah->sc_ah.ah_info.flow_label;
 		if (!memcmp(&ah->sc_ah.ah_info, &new_ah->sc_ah.ah_info,
 			    sizeof(ah->sc_ah.ah_info))) {
-			refcount_inc(&ah->refcnt);
+			if (!ah->refcnt) {
+				/* Item was on the pending deletion list, so
+				 * remove since there's now a new reference to it.
+				 */
+				list_del(&ah->node);
+				iwdev->ah_deletion_list_cnt--;
+			}
+			ah->refcnt++;
 			new_ah->parent_ah = ah;
 			return true;
 		}
 	}
 	new_ah->sc_ah.ah_info.ah_idx = save_ah_id;
+	new_ah->sc_ah.ah_info.flow_label = save_flow_label;
 	/* Add new AH to list */
 	ah = kmemdup(new_ah, sizeof(*new_ah), GFP_KERNEL);
 	if (!ah)
@@ -1041,7 +1051,7 @@ static bool irdma_ah_exists(struct irdma_device *iwdev,
 	iwdev->ah_list_cnt++;
 	if (iwdev->ah_list_cnt > iwdev->ah_list_hwm)
 		iwdev->ah_list_hwm = iwdev->ah_list_cnt;
-	refcount_set(&ah->refcnt, 1);
+	ah->refcnt = 1;
 
 	return false;
 }
@@ -1305,16 +1315,15 @@ int irdma_create_ah_v2(struct ib_ah *ib_ah,
 	if (err)
 		goto err_gid_l2;
 
-	if (sleep) {
-		mutex_lock(&iwdev->ah_tbl_lock);
-		if (irdma_ah_exists(iwdev, ah)) {
-			irdma_free_rsrc(iwdev->rf, iwdev->rf->allocated_ahs, ah_id);
-			ah_id = 0;
+	spin_lock(&iwdev->ah_tbl_lock);
+	if (irdma_ah_exists(iwdev, ah)) {
+		irdma_free_rsrc(iwdev->rf, iwdev->rf->allocated_ahs, ah_id);
+		ah_id = 0;
 #ifdef CONFIG_DEBUG_FS
-			iwdev->ah_reused++;
+		iwdev->ah_reused++;
 #endif
-			goto exit;
-		}
+		goto exit;
+
 	}
 
 	err = irdma_ah_cqp_op(iwdev->rf, sc_ah, IRDMA_OP_AH_CREATE,
@@ -1333,28 +1342,21 @@ exit:
 		uresp.ah_id = ah->sc_ah.ah_info.ah_idx;
 		err = ib_copy_to_udata(udata, &uresp, min(sizeof(uresp), udata->outlen));
 		if (err) {
-			if (!ah->parent_ah ||
-			    (ah->parent_ah && refcount_dec_and_test(&ah->parent_ah->refcnt))) {
-				irdma_ah_cqp_op(iwdev->rf, &ah->sc_ah,
-						IRDMA_OP_AH_DESTROY, false, NULL, ah);
-				ah_id = ah->sc_ah.ah_info.ah_idx;
-				goto err_ah_create;
-			}
-			goto err_unlock;
+			goto err_ah_create;
 		}
 	}
-	if (sleep)
-		mutex_unlock(&iwdev->ah_tbl_lock);
+
+	spin_unlock(&iwdev->ah_tbl_lock);
 	return 0;
 err_ah_create:
-	if (ah->parent_ah) {
-		hash_del(&ah->parent_ah->list);
-		kfree(ah->parent_ah);
-		iwdev->ah_list_cnt--;
+	ah->parent_ah->refcnt--;
+	if (!ah->parent_ah->refcnt) {
+		/* Last ref dropped, add to deferred delete list. */
+		list_add_tail(&ah->parent_ah->node, &iwdev->ah_deletion_list);
+		iwdev->ah_deletion_list_cnt++;
+		ah->parent_ah->deletion_timestamp = ktime_get_raw_ns();
 	}
-err_unlock:
-	if (sleep)
-		mutex_unlock(&iwdev->ah_tbl_lock);
+	spin_unlock(&iwdev->ah_tbl_lock);
 err_gid_l2:
 	if (ah_id)
 		irdma_free_rsrc(iwdev->rf, iwdev->rf->allocated_ahs, ah_id);
@@ -2781,22 +2783,23 @@ int irdma_destroy_ah(struct ib_ah *ibah, u32 ah_flags)
 	struct irdma_device *iwdev = to_iwdev(ibah->device);
 	struct irdma_ah *ah = to_iwah(ibah);
 
-	if (ah->parent_ah) {
-		mutex_lock(&iwdev->ah_tbl_lock);
-		if (!refcount_dec_and_test(&ah->parent_ah->refcnt)) {
-			mutex_unlock(&iwdev->ah_tbl_lock);
-			return 0;
-		}
-		hash_del(&ah->parent_ah->list);
-		kfree(ah->parent_ah);
-		iwdev->ah_list_cnt--;
-		mutex_unlock(&iwdev->ah_tbl_lock);
+	if (!ah->parent_ah) {
+		BUG();
 	}
-	irdma_ah_cqp_op(iwdev->rf, &ah->sc_ah, IRDMA_OP_AH_DESTROY,
-			false, NULL, ah);
 
-	irdma_free_rsrc(iwdev->rf, iwdev->rf->allocated_ahs,
-			ah->sc_ah.ah_info.ah_idx);
+	spin_lock(&iwdev->ah_tbl_lock);
+	ah->parent_ah->refcnt--;
+	if (!ah->parent_ah->refcnt) {
+		/* Last ref dropped, add to deferred delete list. */
+		list_add_tail(&ah->parent_ah->node, &iwdev->ah_deletion_list);
+		iwdev->ah_deletion_list_cnt++;
+		iwdev->ah_deletion_list_cnt_total++;
+		if (iwdev->ah_deletion_list_cnt > iwdev->ah_deletion_list_cnt_peak) {
+			iwdev->ah_deletion_list_cnt_peak = iwdev->ah_deletion_list_cnt;
+		}
+		ah->parent_ah->deletion_timestamp = ktime_get_raw_ns();
+	}
+	spin_unlock(&iwdev->ah_tbl_lock);
 
 	return 0;
 }

rdma-driver/src/irdma/main.c
@@ -648,15 +648,93 @@ static void irdma_poll_cq3(struct irdma_pci_f *rf)
 		cq->ibcq.comp_handler(&cq->ibcq, cq->ibcq.cq_context);
 }
 
+static void ah_purge(struct irdma_device *iwdev)
+{
+	struct list_head *entry;
+	struct list_head *tmp;
+
+	printk(KERN_ERR "Purging all AH entries\n");
+
+	spin_lock(&iwdev->ah_tbl_lock);
+
+	list_for_each_safe(entry, tmp, &iwdev->ah_deletion_list) {
+		struct irdma_ah *ah = container_of(entry, struct irdma_ah,
+						   node);
+		printk(KERN_ERR "Purge AH for PD IDX %u "
+		       "addr: 0x%08x 0x%08x 0x%08x 0x%08x\n",
+		       ah->sc_ah.ah_info.pd_idx,
+		       ah->sc_ah.ah_info.dest_ip_addr[0],
+		       ah->sc_ah.ah_info.dest_ip_addr[1],
+		       ah->sc_ah.ah_info.dest_ip_addr[2],
+		       ah->sc_ah.ah_info.dest_ip_addr[3]);
+
+		irdma_ah_cqp_op(iwdev->rf, &ah->sc_ah, IRDMA_OP_AH_DESTROY,
+				false, NULL, ah);
+
+		irdma_free_rsrc(iwdev->rf, iwdev->rf->allocated_ahs,
+				ah->sc_ah.ah_info.ah_idx);
+
+		hash_del(&ah->list);
+		iwdev->ah_list_cnt--;
+		iwdev->ah_deletion_list_cnt--;
+		list_del(&ah->node);
+		kfree(ah->parent_ah);
+	}
+
+	spin_unlock(&iwdev->ah_tbl_lock);
+}
+
+#define AH_AGE_THRESH_NANOS    5000000000  /* 5 seconds. */
+static void ah_age_out(struct irdma_device *iwdev)
+{
+	u64 now;
+	struct irdma_ah *ah;
+
+	spin_lock(&iwdev->ah_tbl_lock);
+
+	if (list_empty(&iwdev->ah_deletion_list)) {
+		spin_unlock(&iwdev->ah_tbl_lock);
+		return;
+	}
+
+	ah = list_first_entry(&iwdev->ah_deletion_list,
+			      struct irdma_ah,
+			      node);
+
+	/* Read time while holding lock, otherwise, contention
+	 * can result in negative times.
+	 */
+	now = ktime_get_raw_ns();
+
+	if ((now - ah->deletion_timestamp) > AH_AGE_THRESH_NANOS) {
+		irdma_ah_cqp_op(iwdev->rf, &ah->sc_ah, IRDMA_OP_AH_DESTROY,
+				false, NULL, ah);
+
+		irdma_free_rsrc(iwdev->rf, iwdev->rf->allocated_ahs,
+				ah->sc_ah.ah_info.ah_idx);
+
+		hash_del(&ah->list);
+		iwdev->ah_list_cnt--;
+		iwdev->ah_deletion_list_cnt--;
+		list_del(&ah->node);
+		kfree(ah->parent_ah);
+	}
+
+	spin_unlock(&iwdev->ah_tbl_lock);
+}
+
 static int poll_thread(void *context)
 {
 	struct irdma_pci_f *rf = context;
 
 	msleep(200);
 
-	rf->sc_dev.last_cqp_poll_ts = ktime_get_ns();
+	rf->sc_dev.last_cqp_poll_ts = ktime_get_raw_ns();
 	do {
 		msleep(1);
+
+		ah_age_out(rf->iwdev);
+
 		if (rf->sc_dev.hw_wa & AEQ_POLL) {
 			irdma_process_aeq(rf);
 			continue;
@@ -664,12 +742,12 @@ static int poll_thread(void *context)
 		if (rf->sc_dev.hw_wa & CCQ_CQ3_POLL) {
 			struct irdma_sc_cq *ccq = &rf->ccq.sc_cq;
 
-			const u64 tmp = ktime_get_ns();
-			const u64 dur = tmp - rf->sc_dev.last_cqp_poll_ts;
+			const u64 now = ktime_get_raw_ns();
+			const u64 dur = now - rf->sc_dev.last_cqp_poll_ts;
 			if (dur > rf->sc_dev.peak_cqp_poll_interval)
 				rf->sc_dev.peak_cqp_poll_interval = dur;
 
-			rf->sc_dev.last_cqp_poll_ts = tmp;
+			rf->sc_dev.last_cqp_poll_ts = now;
 
 			if (ccq)
 				irdma_cqp_ce_handler(rf, ccq);
@@ -738,6 +816,8 @@ static void irdma_remove(struct auxiliary_device *aux_dev)
 	}
 
 	irdma_ib_unregister_device(iwdev);
+	/* Delete all AH entries on the age-out list. */
+	ah_purge(iwdev);
 	irdma_unregister_notifiers(iwdev);
 	irdma_deinit_device(iwdev);
 	ib_dealloc_device(&iwdev->ibdev);
@@ -856,8 +936,9 @@ static int irdma_fill_device_info(struct irdma_device *iwdev, struct iidc_core_d
 	/* Can override limits_sel, protocol_used */
 	irdma_set_rf_user_cfg_params(rf);
 
-	mutex_init(&iwdev->ah_tbl_lock);
-
+	spin_lock_init(&iwdev->ah_tbl_lock);
+	mutex_init(&iwdev->delete_lock);
+	INIT_LIST_HEAD(&iwdev->ah_deletion_list);
 	iwdev->netdev = cdev_info->netdev;
 	iwdev->vsi_num = cdev_info->vport_id;
 	iwdev->init_state = INITIAL_STATE;
@@ -912,6 +993,8 @@ static int irdma_probe(struct auxiliary_device *aux_dev, const struct auxiliary_
 	int err;
 	struct irdma_handler *hdl;
 
+	printk(KERN_ERR "AH dedup and deferred deletion enabled\n");
+
 	if (cdev_info->ver.major != IIDC_MAJOR_VER) {
 		pr_err("version mismatch:\n");
 		pr_err("expected major ver %d, caller specified major ver %d\n",
@@ -957,10 +1040,8 @@ static int irdma_probe(struct auxiliary_device *aux_dev, const struct auxiliary_
 	if (irdma_fw_major_ver(&rf->sc_dev) == 2 && mod_rdpu_bw)
 		irdma_modify_rdpu_bw(rf);
 
-	if (rf->rdma_ver >= IRDMA_GEN_3 &&
-	    rf->sc_dev.hw_wa & TIMER_NEEDED)
-		rf->poll_thread =
-			kthread_run(poll_thread, rf, "dpc polling thread");
+	rf->poll_thread =
+		kthread_run(poll_thread, rf, "dpc polling thread");
 
 	if (rf->rdma_ver == IRDMA_GEN_2) {
 		if (irdma_set_attr_from_fragcnt(&rf->sc_dev, rf->fragcnt_limit))

rdma-driver/src/irdma/debugfs.c
@@ -931,6 +931,12 @@ static void dump_stats_cmd(struct irdma_device *iwdev)
 		      iwdev->ah_list_cnt);
 	dbg_vsnprintf("AH list cnt HWM                  %d\n",
 		      iwdev->ah_list_hwm);
+	dbg_vsnprintf("AH deletion list cnt curr        %u\n",
+		      iwdev->ah_deletion_list_cnt);
+	dbg_vsnprintf("AH deletion list cnt total       %u\n",
+		      iwdev->ah_deletion_list_cnt_total);
+	dbg_vsnprintf("AH deletion list cnt peak        %u\n",
+		      iwdev->ah_deletion_list_cnt_peak);
 
 	dbg_vsnprintf("MAD QP Posted Send WRs           %d\n",
 		      atomic_read(&iwdev->mad_qp_stats.posted_send_wrs));

rdma-driver/src/irdma/hw.c
@@ -171,7 +171,7 @@ static void irdma_complete_cqp_request(struct irdma_cqp *cqp,
 				       struct irdma_cqp_request *cqp_request)
 {
 	struct irdma_sc_dev* dev = cqp->sc_cqp.dev;
-	const u64 duration = ktime_get_ns() - cqp_request->submission_ts;
+	const u64 duration = ktime_get_raw_ns() - cqp_request->submission_ts;
 
 	if (duration > 2000000000) dev->cqp_cmds_latency_2s++;
 	if (duration > dev->cqp_cmd_peak_latency[cqp_request->info.cqp_cmd])
@@ -1984,7 +1984,7 @@ void irdma_ctrl_deinit_hw(struct irdma_pci_f *rf)
 {
 	enum init_completion_state state = rf->init_state;
 
-	if ((rf->sc_dev.hw_wa & TIMER_NEEDED) && rf->poll_thread) {
+	if (rf->poll_thread) {
 		kthread_stop(rf->poll_thread);
 		rf->poll_thread = NULL;
 	}

rdma-driver/src/irdma/main.h
@@ -418,7 +418,12 @@ struct irdma_device {
 	struct irdma_cm_core cm_core;
 	struct irdma_ae_info ae_info;
 	DECLARE_HASHTABLE(ah_hash_tbl, 8);
-	struct mutex ah_tbl_lock;
+	struct list_head ah_deletion_list;
+	u32 ah_deletion_list_cnt;
+	u32 ah_deletion_list_cnt_total;
+	u32 ah_deletion_list_cnt_peak;
+	spinlock_t ah_tbl_lock;
+	struct mutex delete_lock;
 #ifdef CONFIG_DEBUG_FS
 	u64 ah_reused;
 #endif

rdma-driver/src/irdma/utils.c
@@ -1083,7 +1083,7 @@ int irdma_handle_cqp_op(struct irdma_pci_f *rf,
 	if (rf->reset)
 		return (info->create ? -EBUSY : 0);
 
-	cqp_request->submission_ts = ktime_get_ns();
+	cqp_request->submission_ts = ktime_get_raw_ns();
 	irdma_get_cqp_request(cqp_request);
 	status = irdma_process_cqp_cmd(dev, info);
 	if (status)

rdma-driver/src/irdma/verbs.h
@@ -81,8 +81,10 @@ struct irdma_ah {
 	u8 sgid_index;
 	union ib_gid dgid;
 	struct hlist_node list;
-	refcount_t refcnt;
+	u64 refcnt;
 	struct irdma_ah *parent_ah;	/* AH from cached list */
+	struct list_head node;
+	u64 deletion_timestamp;
 };
 
 struct irdma_hmc_pble {

commit bd6433d55f5c18e7bbc5d7985f8c442a5feab867

irdma: fix more sleep-while-atomic issues
There were a few places that were missed.

Fixes: 28d5244aaa ("irdma: enable full AH dedup with deferred deletion")
Signed-off-by: Jacob Moroni <jmoroni@google.com>

rdma-driver/src/irdma/irdma_kcompat.c
@@ -1042,7 +1042,7 @@ static bool irdma_ah_exists(struct irdma_device *iwdev,
 	new_ah->sc_ah.ah_info.ah_idx = save_ah_id;
 	new_ah->sc_ah.ah_info.flow_label = save_flow_label;
 	/* Add new AH to list */
-	ah = kmemdup(new_ah, sizeof(*new_ah), GFP_KERNEL);
+	ah = kmemdup(new_ah, sizeof(*new_ah), GFP_ATOMIC);
 	if (!ah)
 		return false;
 	new_ah->parent_ah = ah;
@@ -1328,15 +1328,19 @@ int irdma_create_ah_v2(struct ib_ah *ib_ah,
 	err = irdma_ah_cqp_op(iwdev->rf, sc_ah, IRDMA_OP_AH_CREATE,
 			      false, NULL, sc_ah);
 	if (err) {
-		ibdev_dbg(&iwdev->ibdev, "CQP-OP Create AH fail");
+		ibdev_err(&iwdev->ibdev, "CQP-OP Create AH fail");
+		spin_unlock(&iwdev->ah_tbl_lock);
 		goto err_ah_create;
 	}
 
 	err = irdma_create_ah_wait(rf, sc_ah, false);
-	if (err)
-		goto err_gid_l2;
+	if (err) {
+		spin_unlock(&iwdev->ah_tbl_lock);
+		goto err_ah_create;
+	}
 
 exit:
+	spin_unlock(&iwdev->ah_tbl_lock);
 	if (udata) {
 		uresp.ah_id = ah->sc_ah.ah_info.ah_idx;
 		err = ib_copy_to_udata(udata, &uresp, min(sizeof(uresp), udata->outlen));
@@ -1345,17 +1349,10 @@ exit:
 		}
 	}
 
-	spin_unlock(&iwdev->ah_tbl_lock);
 	return 0;
 err_ah_create:
-	ah->parent_ah->refcnt--;
-	if (!ah->parent_ah->refcnt) {
-		/* Last ref dropped, add to deferred delete list. */
-		list_add_tail(&ah->parent_ah->node, &iwdev->ah_deletion_list);
-		iwdev->ah_deletion_list_cnt++;
-		ah->parent_ah->deletion_timestamp = ktime_get_raw_ns();
-	}
-	spin_unlock(&iwdev->ah_tbl_lock);
+	irdma_destroy_ah(ib_ah, 0);
+	return err;
 err_gid_l2:
 	if (ah_id)
 		irdma_free_rsrc(iwdev->rf, iwdev->rf->allocated_ahs, ah_id);

rdma-driver/src/irdma/main.c
@@ -993,7 +993,7 @@ static int irdma_probe(struct auxiliary_device *aux_dev, const struct auxiliary_
 	int err;
 	struct irdma_handler *hdl;
 
-	printk(KERN_ERR "AH dedup and deferred deletion enabled\n");
+	printk(KERN_ERR "AH dedup and deferred deletion enabled (v2)\n");
 
 	if (cdev_info->ver.major != IIDC_MAJOR_VER) {
 		pr_err("version mismatch:\n");